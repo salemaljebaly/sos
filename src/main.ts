13926dd0bd8b8426ec68ead452f87dae3431af8b
Salem Aljebaly <56629923+salemaljebaly@users.noreply.github.com>
2022-02-23 04:12:54 +0200

    fix class validator problem and with add useGlobalPipes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -15,7 +16,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
-
+  // add Global pipes to pass class validator
+  
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
